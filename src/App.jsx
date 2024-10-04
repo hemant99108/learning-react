2b30fdfcd04e657ffbb9803193ce15125a6352f8
Hemant <kumarhemant99108@gmail.com>
2024-10-04 10:20:53 +0530

    added another branch for the sheriyans code practice

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import "./App.css";
+import Displaycontent from './Displaycontent';
+
+function App() {
+  const [val, setval] = useState({ name: "", address:""});
+  const [data,setdata]=useState([]);//take an array and populate over there 
+
+  const[errors,setErrors]=useState({});
+
+
+  const validate=()=>{
+    let tempErrors={};
+
+    if(!val.name)tempErrors.name="name is required";
+    if(!val.address)tempErrors.address="address is required";
+
+
+    setErrors(tempErrors);
+    return Object.keys(tempErrors).length===0;  //return true if no errors 
+  }
+
+
+  const handlechange=(e)=>{
+    const {name,value}=e.target;
+
+     setval((val)=>({
+      ...val,
+      [name]:value
+     }))
+  }
+
+  const handlesubmit=(e)=>{
+      e.preventDefault();
+
+      if(validate()){
+        setdata((prev)=>[...prev,val]);
+        setval({name:"",address:""});//clear after submission 
+        setErrors({});
+      }
+  }
+
+   return (
+
+      <>
+
+        <div className="px-3 py-3 bg-slate-50 flex justify-between ">
+
+        {errors.name && <span className="error-text">{errors.name}</span>}
+        {errors.address && <span className="error-text">{errors.address}</span>}
+
+            <label  >name</label>
+            <input type="text"  name="name" value={val.name} onChange={handlechange}  />  
+            <label >address</label> 
+            <input type="text" name="address" value={val.address} onChange={handlechange}  /> 
+        </div>
+
+          <button className=" bg-blue-300 px-3 py-3 " type="submit" 
+          onClick={handlesubmit}  
+        >populate data</button>
+
+
+
+      <Displaycontent props={data}/>
+
+      </>
+
+   )
+
+  }
+
+export default App;
+
+
+
---
+++ src/Displaycontent.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+ 
+
+function Displaycontent({props}) {
+
+     
+  return (
+    <div>
+       <h3>Displaying Multiple Names and Addresses</h3>
+
+            {
+            props.length>0? (
+                props.map((prop, index) => {
+          return (
+            <div key={index}>
+              <h1>{prop.name}</h1>
+              <h2>{prop.address}</h2>
+            </div>
+          );
+        })
+            ):(
+                <p>No data to display</p>
+            )
+        }
+     </div>
+  ) 
+}
+
+export default Displaycontent ;
